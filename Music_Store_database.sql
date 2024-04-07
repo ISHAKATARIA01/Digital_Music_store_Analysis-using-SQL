37e4a383363129b4b0572b08041271c20bbf8f73
Isha Kataria <127096168+ISHAKATARIA01@users.noreply.github.com>
2024-04-07 15:04:29 +0530

    Add files via upload

---
+++ Music_Store_database.sql
@@ -0,0 +1,14 @@
+-- Database: Music_Database
+
+-- DROP DATABASE IF EXISTS "Music_Database";
+
+CREATE DATABASE "Music_Database"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
